10d69e21c7cec23f4f4e1ecddfa11d0f0cbd9cb1
mgwilliams112-ui <mgwilliams112@gmail.com>
2026-06-26 12:51:10 -0400

    Refactor URL patterns to use class-based views

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -1,13 +1,13 @@
-from django.urls import path
-from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
 app_name = 'onlinecourse'
 urlpatterns = [
-    path(route='course/<int:course_id>/', view=views.course_details, name='course_details'),
-    path(route='course/<int:course_id>/enroll/', view=views.enroll, name='enroll'),
-    path(route='', view=views.popular_course_list, name='popular_course_list')
+    path(route='', view=views.CourseListView.as_view(), name='popular_course_list'),
+    path(route='course/<int:pk>/enroll/', view=views.EnrollView.as_view(), name='enroll'),
+    path(route='course/<int:pk>/', view=views.CourseDetailsView.as_view(), name='course_details')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
 
